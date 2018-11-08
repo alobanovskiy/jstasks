8793ed34c9a477f1cfbc55fd3a1eee63ed219fc2
alobanovskiy <alobanovskiy@biarum.com>
2018-11-08 13:49:15 +0300

    some fixes

--- task2/task2.js
+++ task2/task2.js
@@ -1,32 +1,12 @@
     function checkFrame(arrayOfChecks){
       let frameDelta = 10;
-      let minXobj = arrayOfChecks.reduce(function min(minV, current){
-        if(minV["x"] < current["x"])
-          return minV;
-        else
-          return current;
-      });
-      let minYobj = arrayOfChecks.reduce(function min(minV, current){
-        if(minV["y"] < current["y"])
-          return minV;
-        else
-          return current;
-      });
-      let maxXobj = arrayOfChecks.reduce(function max(maxV, current){
-        if((maxV["x"] + maxV["width"]) > (current["x"] + current["width"]))
-          return maxV;
-        else
-          return current;
-      });
-      let maxYobj = arrayOfChecks.reduce(function max(maxV, current){
-        if((maxV["y"] + maxV["height"]) > (current["y"] + current["height"]))
-          return maxV;
-        else
-          return current;
-      });
-      let cordX = minXobj["x"] - frameDelta;
-      let cordY = maxYobj["y"] + frameDelta;
-      let width = maxXobj["x"] - cordX + frameDelta + maxXobj["width"];
-      let height = cordY - minYobj["y"] + frameDelta + minYobj["height"];
+      let minX = Math.min(...arrayOfChecks.map(function check(check){return check.x}));
+      let minY = Math.min(...arrayOfChecks.map(function check(check){return check.y}));
+      let maxY = Math.max(...arrayOfChecks.map(function check(check){return check.y + check.height}));
+      let maxX = Math.max(...arrayOfChecks.map(function check(check){return check.x + check.width}));
+      let cordX = minX - frameDelta;
+      let cordY = minY - frameDelta;
+      let width = max - cordX + frameDelta;
+      let height = maxY - cordY + frameDelta;
       return { x: cordX, y: cordY, width: width, height: height };
     }
